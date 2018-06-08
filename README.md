785229c1d02352471463402e573d1b85f58ce5a5
chacewang <chacewang@tencent.com>
2018-06-08 15:04:56 +0800

    工程代码初始化

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # xinge_android_demo
+
+这是AndroidStudio工程集成信鸽SDK的demo
+详细集成方法参考[官方集成文档](http://docs.developer.qq.com/xg/android_access/jcenter.html)
---
+++ app/src/main/java/com/qq/xgdemo/app.java
@@ -0,0 +1,20 @@
+package com.qq.xgdemo;
+
+import android.app.Application;
+
+
+/**
+ * Created by admin on 2017/2/13.
+ */
+
+public class app extends Application {
+    @Override
+    public void onCreate() {
+        // TODO Auto-generated method stub
+        super.onCreate();
+    }
+
+}
+
+
+
